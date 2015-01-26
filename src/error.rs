e36e7b0cc4bbc6f666db5f8f6367fed9f6d76e25
Sven Nilsen <bvssvni@gmail.com>
2015-01-26 14:41:51 +0100

    Upgraded to latest Rust/freetype-rs

--- src/error.rs
+++ src/error.rs
@@ -11,8 +11,6 @@ pub enum Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use std::fmt::Show;
-
-        self.fmt(f)
+        fmt::Debug::fmt(self, f)
     }
 }
