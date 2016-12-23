PistonDevelopers/opengl_graphics

commit 4ae01a95d0e958461551104fe709d69f582c0500

Fixed glyph artifacts
- Bumped to 0.36.1
- Added “text_test” example

src/glyph_cache.rs
@@ -123,20 +123,20 @@ impl<'b> CharacterCache for GlyphCache<'b> {
                 let bounding_box = glyph.exact_bounding_box().unwrap_or(rt::Rect{min: rt::Point{x: 0.0, y: 0.0}, max: rt::Point{x: 0.0, y: 0.0} });
                 let glyph = glyph.positioned(rt::point(0.0, 0.0));
                 let pixel_bounding_box = glyph.pixel_bounding_box().unwrap_or(rt::Rect{min: rt::Point{x: 0, y: 0}, max: rt::Point{x: 0, y: 0} });
-                let pixel_bb_width = pixel_bounding_box.width();
-                let pixel_bb_height = pixel_bounding_box.height();
+                let pixel_bb_width = pixel_bounding_box.width() + 2;
+                let pixel_bb_height = pixel_bounding_box.height() + 2;
 
                 let mut image_buffer = Vec::<u8>::new();
                 image_buffer.resize((pixel_bb_width * pixel_bb_height) as usize, 0);
                 glyph.draw(|x, y, v| {
-                   let pos = (x + y * (pixel_bb_width as u32)) as usize;
+                   let pos = ((x+1) + (y+1) * (pixel_bb_width as u32)) as usize;
                    image_buffer[pos] = (255.0 * v) as u8;
                 });
 
                 let &mut (offset, size, ref texture) = v.insert((
                     [
-                        bounding_box.min.x as Scalar,
-                        -pixel_bounding_box.min.y as Scalar,
+                        bounding_box.min.x as Scalar + 1.0,
+                        -pixel_bounding_box.min.y as Scalar + 1.0,
                     ],
                     [
                         h_metrics.advance_width as Scalar,

Cargo.toml
@@ -1,7 +1,7 @@
 [package]
 
 name = "piston2d-opengl_graphics"
-version = "0.36.0"
+version = "0.36.1"
 authors = [
     "bvssvni <bvssvni@gmail.com>",
     "Coeuvre <coeuvre@gmail.com>",

examples/text_test.rs
@@ -1,0 +1,41 @@
+extern crate piston;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate sdl2_window;
+
+use piston::window::{ WindowSettings, Size };
+use piston::event_loop::*;
+use piston::input::*;
+use opengl_graphics::*;
+use opengl_graphics::glyph_cache::GlyphCache;
+use sdl2_window::Sdl2Window;
+
+fn main() {
+    let opengl = OpenGL::V3_2;
+    let size = Size { width: 500, height: 300 };
+    let ref mut window: Sdl2Window =
+        WindowSettings::new("opengl_graphics: text_test", size)
+        .exit_on_esc(true).opengl(opengl).build().unwrap();
+
+    let mut glyph_cache = GlyphCache::new("assets/FiraSans-Regular.ttf")
+        .unwrap();
+
+    let mut gl = GlGraphics::new(opengl);
+    let mut events = window.events();
+    while let Some(e) = events.next(window) {
+        if let Some(args) = e.render_args() {
+            gl.draw(args.viewport(), |c, g| {
+                use graphics::*;
+
+                clear([1.0; 4], g);
+                text::Text::new_color([0.0, 0.5, 0.0, 1.0], 32).draw(
+                    "Hello opengl_graphics!",
+                    &mut glyph_cache,
+                    &DrawState::default(),
+                    c.transform.trans(10.0, 100.0),
+                    g
+                );
+            });
+        }
+    }
+}

commit c3c64d777e49f6b680657fc4fee1301cc04a223c

Fix bug causing text to be drawn 2 pixels off
It appears that 4ae01a95d0e958461551104fe709d69f582c0500 introduced a bug
causing text to be drawn 2 pixels to the right of where it should be.

src/glyph_cache.rs
@@ -135,7 +135,7 @@ impl<'b> CharacterCache for GlyphCache<'b> {
 
                 let &mut (offset, size, ref texture) = v.insert((
                     [
-                        bounding_box.min.x as Scalar + 1.0,
+                        bounding_box.min.x as Scalar - 1.0,
                         -pixel_bounding_box.min.y as Scalar + 1.0,
                     ],
                     [