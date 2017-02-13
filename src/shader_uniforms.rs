dc835f4871eef654ac6bcacfd16c60562eaa2b77
Daniel Slocombe <ds4314@ic.ac.uk>
2017-02-13 07:00:02 +0000

    Document code, add matrix types and methods

--- src/shader_uniforms.rs
+++ src/shader_uniforms.rs
@@ -1,37 +1,59 @@
-//! OpenGL back-end for Piston-Graphics.
+//! Types and methods for setting shader uniforms
 
 
 // External crates.
 use std::ffi::CString;
 use gl;
-use gl::types::{GLint};
+use gl::types::{GLboolean, GLint};
 use std::marker::PhantomData;
 
 // Local crate.
 use back_end::GlGraphics;
 
-/// TODO
-pub trait UniformType {}
-/// TODO
-pub trait SUFloat : UniformType {}
-/// TODO
-pub trait SUInt : UniformType {}
-/// TODO
-pub trait SUVec2 : UniformType {}
-/// TODO
-pub trait SUVec3 : UniformType {}
-/// TODO
-pub trait SUVec4 : UniformType {}
-
-/// TODO
+/// Describes a shader uniform of a given type.
 #[derive(Clone, Copy)]
 pub struct ShaderUniform<T : ?Sized>{
     location : GLint,
     phantom : PhantomData<T>,
 }
 
+/// Shader uniform type
+///
+/// For now a small subset
+pub trait UniformType {}
+
+/// Shader uniform float
+pub trait SUFloat : UniformType {}
+
+/// Shader uniform integer
+pub trait SUInt : UniformType {}
+
+/// Shader uniform vector of size 2
+/// Vector elements are floats
+pub trait SUVec2 : UniformType {}
+
+/// Shader uniform vector of size 3
+/// Vector elements are floats
+pub trait SUVec3 : UniformType {}
+
+/// Shader uniform vector of size 4
+/// Vector elements are floats
+pub trait SUVec4 : UniformType {}
+
+/// Shader uniform 2x2 matrix
+/// Matrix elements are floats
+pub trait SUMat2x2 : UniformType {}
+
+/// Shader uniform 3x3 matrix
+/// Matrix elements are floats
+pub trait SUMat3x3 : UniformType {}
+
+/// Shader uniform 4x4 matrix
+/// Matrix elements are floats
+pub trait SUMat4x4 : UniformType {}
+
 impl GlGraphics {
-/// TODO
+    /// Try to get uniform from the current shader of a given name.
     pub fn get_uniform<T : UniformType + ?Sized>(&self, name : &str) -> Option<ShaderUniform<T>> {
         self.get_current_program().and_then( |p| {
             match unsafe {gl::GetUniformLocation(p, CString::new(name).unwrap().
@@ -49,7 +71,7 @@ impl GlGraphics {
 }
 
 impl ShaderUniform<SUFloat> {
-/// TODO
+    /// Set the value of the float uniform.
     pub fn set(&self, gl : &GlGraphics, value : f32) {
         gl.get_current_program().map(|p| {
             unsafe {gl::ProgramUniform1f(p, self.location, value)}
@@ -58,7 +80,7 @@ impl ShaderUniform<SUFloat> {
 }
 
 impl ShaderUniform<SUInt> {
-/// TODO
+    /// Set the value of the integer uniform.
     pub fn set(&self, gl : &GlGraphics, value : i32) {
         gl.get_current_program().map(|p| {
             unsafe {gl::ProgramUniform1i(p, self.location, value)}
@@ -67,7 +89,7 @@ impl ShaderUniform<SUInt> {
 }
 
 impl ShaderUniform<SUVec2> {
-/// TODO
+    /// Set the value of the vector 2 uniform.
     pub fn set(&self, gl : &GlGraphics, value : &[f32; 2]) {
         gl.get_current_program().map(|p| {
             unsafe {gl::ProgramUniform2f(p, self.location, value[0], value[1])}
@@ -76,7 +98,7 @@ impl ShaderUniform<SUVec2> {
 }
 
 impl ShaderUniform<SUVec3> {
-/// TODO
+    /// Set the value of the vector 3 uniform.
     pub fn set(&self, gl : &GlGraphics, value : &[f32; 3]) {
         gl.get_current_program().map(|p| {
             unsafe {gl::ProgramUniform3f(p, self.location, value[0], value[1], value[2])}
@@ -85,7 +107,7 @@ impl ShaderUniform<SUVec3> {
 }
 
 impl ShaderUniform<SUVec4> {
-/// TODO
+    /// Set the value of the vector 4 uniform.
     pub fn set(&self, gl : &GlGraphics, value : &[f32; 4]) {
         gl.get_current_program().map(|p| {
             unsafe {gl::ProgramUniform4f(p, self.location, value[0], value[1], value[2], value[3])}
@@ -93,3 +115,29 @@ impl ShaderUniform<SUVec4> {
     }
 }
 
+impl ShaderUniform<SUMat2x2> {
+    /// Set the value of the 2x2 matrix uniform.
+    pub fn set(&self, gl : &GlGraphics, values : &[f32; 4]) {
+        gl.get_current_program().map(|p| {
+            unsafe {gl::ProgramUniformMatrix2fv(p, self.location, 1 as GLint, false as GLboolean, values.as_ptr())}
+        });
+    }
+}
+
+impl ShaderUniform<SUMat3x3> {
+    /// Set the value of the 3x3 matrix uniform.
+    pub fn set(&self, gl : &GlGraphics, values : &[f32; 9]) {
+        gl.get_current_program().map(|p| {
+            unsafe {gl::ProgramUniformMatrix3fv(p, self.location, 1 as GLint, false as GLboolean, values.as_ptr())}
+        });
+    }
+}
+
+impl ShaderUniform<SUMat4x4> {
+    /// Set the value of the 4x4 matrix uniform.
+    pub fn set(&self, gl : &GlGraphics, values : &[f32; 16]) {
+        gl.get_current_program().map(|p| {
+            unsafe {gl::ProgramUniformMatrix4fv(p, self.location, 1 as GLint, false as GLboolean, values.as_ptr())}
+        });
+    }
+}
